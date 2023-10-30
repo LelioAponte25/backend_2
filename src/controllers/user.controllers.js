2ff8b12986de124464f03d4316223ca9893cf9cd
Lelio Aponte <apontelelio6@gmail.com>
2023-10-30 17:39:14 -0500

    1

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,59 @@
+const catchError = require('../utils/catchError');
+const User = require('../models/User');
+
+const getAll = catchError(async(req, res) => {
+    const users = await User.findAll();
+    return res.json(users)
+});
+
+//create users 
+const create = catchError(async(req, res) =>{
+    const {first_name, last_name, email, password, birthday } = req.body;
+    const user = await User.create({
+        first_name,
+        last_name,
+        email,
+        password,
+        birthday,
+    });
+    return res.status(201).json(user);
+});
+
+//atraer el userio por id
+const getOne_2 = catchError(async(req, res) =>{
+    const { id }  = req.params;
+    const user = await User.findOne({where:{id}});
+    return  res.json(user)
+})
+
+//delete for id
+const remove = catchError(async(req, res) => {
+    const { id }  = req.params;
+    await User.destroy({where: {id}})
+    return res.sendStatus(204)
+})
+
+const update = catchError(async(req, res) => {
+    const { id }  = req.params;
+    const {first_name, last_name, email, password, birthday } = req.body;
+    const user = await User.update(
+    {   first_name,
+        last_name,
+        email,
+        password,
+        birthday},
+
+        {where: {id}, returning:true}
+        
+    )
+    return res.json(user)
+})
+
+
+module.exports = {
+    getAll,
+    create,
+    getOne_2,
+    remove,
+    update,
+}
---
+++ src/models/User.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utils/connection'); 
+   // En Mayúsculas y singular      // en minúsculas y singular
+const User = sequelize.define('user', {
+    // Definimos las columnas aquí
+    first_name: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    last_name: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING(200),
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING(32),
+        allowNull: false
+    },
+    birthday: {
+        type: DataTypes.DATE,
+        allowNull: false
+    }
+});
+
+module.exports = User;
